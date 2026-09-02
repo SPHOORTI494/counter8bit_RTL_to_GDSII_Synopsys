7beb4f31e70311f6205623174429c035a74ae715
Sphoorti <147730649+SPHOORTI494@users.noreply.github.com>
2026-09-02 10:12:54 +0530

    Create star.cmd

---
+++ BackEnd/RC Extraction/star.cmd
@@ -0,0 +1,21 @@
+* StarRC command file for 8-bit counter
+* SAED32 RVT / 1P9M
+
+TCAD_GRD_FILE: /home/synopsys/synopsys_tools/tech_libs/lib32nm/edk32nm/SAED32_EDK/tech/star_rcxt/saed32nm_1p9m_nominal.nxtgrd
+
+MAPPING_FILE: /home/synopsys/synopsys_tools/tech_libs/lib32nm/edk32nm/SAED32_EDK/tech/star_rcxt/saed32nm_tf_itf_tluplus.map
+
+LEF_FILE: /home/synopsys/synopsys_tools/tech_libs/lib32nm/edk32nm/SAED32_EDK/lib/oa/saed32_sram_lp_dual_oa/new/newtech.lef /home/synopsys/synopsys_tools/tech_libs/lib32nm/edk32nm/SAED32_EDK/lib/stdcell_rvt/SAED32_EDK/lib/stdcell_rvt/lef/saed32nm_rvt_1p9m.lef
+
+TOP_DEF_FILE: /home/synopsys/Sphoorti2/backEnd/scripts/routing_finished.def
+
+NETLIST_FILE: /home/synopsys/Sphoorti2/backEnd/scripts/counter8bit.spef
+
+NETLIST_FORMAT: SPEF
+
+EXTRACTION: RC
+
+OPERATING_TEMPERATURE: 25
+
+REM newtech.lef = technology LEF
+REM nested RVT saed32nm_rvt_1p9m.lef = standard-cell macro LEF
